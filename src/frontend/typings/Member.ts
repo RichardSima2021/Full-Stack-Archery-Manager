9054bf1a46c4fe68c44f1fc4198240928f7ce5fd
richardsima <biscuitsima@tencent.com>
2023-09-19 20:05:22 +0800

    feat: updated member typing to match db

--- src/frontend/typings/Member.ts
+++ src/frontend/typings/Member.ts
@@ -1,18 +1,24 @@
 class Member {
+  id?: number;
   studentId: string;
   fullName: string;
   email: string;
+  active?: boolean;
   owesEquipment: boolean;
 
   constructor(
+    id: number,
     studentId: string,
     fullName: string,
     email: string,
+    active: boolean,
     owesEquipment: boolean
   ) {
+    this.id = id;
     this.studentId = studentId;
     this.fullName = fullName;
     this.email = email;
+    this.active = active;
     this.owesEquipment = owesEquipment;
   }
 }
