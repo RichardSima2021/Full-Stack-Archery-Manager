3296caabc520fdcae1a30e7226b5b0f5b6fe3cb4
richardsima <biscuitsima@tencent.com>
2023-09-19 17:14:06 +0800

    chore: setting up backend environment

---
+++ src/backend/spring-boot-library/src/main/java/com/archeryclubmanager/springbootlibrary/entity/Member.java
@@ -0,0 +1,32 @@
+package com.archeryclubmanager.springbootlibrary.entity;
+
+import lombok.Data;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="members")
+@Data
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="firstName")
+    private String firstName;
+
+    @Column(name="lastName")
+    private String lastName;
+
+    @Column(name="studentId")
+    private Long studentId;
+
+    @Column(name="uciEmail")
+    private String uciEmail;
+
+    @Column(name="active")
+    private boolean active;
+
+    @Column(name="loaningEquipment")
+    private boolean loaningEquipment;
+}
